237a63460086c9179b0a18c4394b5d1f8df89f1f
melodycurran <curranmelody02@gmail.com>
2024-01-23 21:17:56 -0700

    Added MVC functionality and added database connection

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +28,8 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 
 //Index route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
