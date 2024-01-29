803e440fcd6cf576d79cbd661847cff9e2eb0102
melodycurran <curranmelody02@gmail.com>
2024-01-28 18:27:14 -0700

    Added error handlers

--- controllers/invController.js
+++ controllers/invController.js
@@ -12,7 +12,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
     const grid = await utilities.buildClassificationGrid(data);
     let nav = await utilities.getNav();
     const className = data[0].classification_name;
-    res.render("./inventory/classification", {title: className + "vehicles", nav, grid, });
+    res.render("./inventory/classification", {title: className + " vehicles", nav, grid, });
 }
 
 module.exports = invCont;
