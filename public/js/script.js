74e6ff73e8fb79340f011d6f62c79035e20c7fb2
melodycurran <curranmelody02@gmail.com>
2024-02-01 20:51:14 -0700

    Added clien-side validation

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
-let styleJs = () => {
-    let pwVisibilityBtn = document.querySelectorAll('.material-symbols-outlined');
+
+function styleJs() {
+    let pwVisibilityBtn = document.querySelectorAll('.visibility-icon');
     let showPw = document.querySelector('#on');
     let hidePw = document.querySelector('#off');
     let pw = document.querySelector('#password');
@@ -21,4 +22,38 @@ let styleJs = () => {
     
 }
 
+function clientValidation() {
+    let registerBtn = document.querySelector('#register-btn');
+    let inputs = document.querySelectorAll('.form-input');
+
+    registerBtn.addEventListener('click', () => {
+        inputs.forEach(el => {
+            let id = el.getAttribute("id");
+            let inputEl = document.querySelector(`#${id}`);
+            validateInputs(inputEl);
+        })
+    })
+
+    function validateInputs(input) {
+        let validation = input.parentElement;
+        if (input.value === "") {
+            validation.className = 'form-validation invalid'
+        } else {
+            validation.className = 'form-validation valid'
+            console.log(input.getAttribute("id"));
+            if (input.getAttribute("id") === "password") {
+                let isMatched = validatePassword(input.value);
+                if (!isMatched) validation.className = 'form-validation invalid';
+            }      
+        }
+    }
+
+    function validatePassword(pw) {
+        const regex = new RegExp('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{12,}$');
+        return regex.test(pw);
+    }
+}
+
+
 styleJs();
+clientValidation();
